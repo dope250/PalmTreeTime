ca924004d4d525d38f9a536c652f963eac87299a
dope250 <dope250@electronicping.net>
2023-09-15 20:57:12 +0200

    Added basic functions

--- Program.cs
+++ Program.cs
@@ -3,17 +3,54 @@ using System.Diagnostics;
 using Microsoft.Win32;
 using System.Drawing;
 using System.Timers;
+using System.Runtime.InteropServices;
 
 class PalmTreeTime
 {
-    static System.Timers.Timer checkTimer;
+    private const int MF_BYCOMMAND = 0x00000000;
+    public const int SC_CLOSE = 0xF060;
+
+    [DllImport("user32.dll")]
+    public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+    [DllImport("kernel32.dll", ExactSpelling = true)]
+    private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetWindowPos(
+        IntPtr hWnd,
+        IntPtr hWndInsertAfter,
+        int x,
+        int y,
+        int cx,
+        int cy,
+        int uFlags);
+
+    private const int HWND_TOPMOST = -1;
+    private const int SWP_NOMOVE = 0x0002;
+    private const int SWP_NOSIZE = 0x0001;
+
+    [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
+    private const int SW_SHOWNORMAL = 1;
+    private const int SW_MINIMIZE = 6;
+
+    static System.Timers.Timer checkTimer = new System.Timers.Timer(60000);
     static int maxTime;
     static DateTime lastTimeUsed;
     static DateTime currentTime;
     static bool debug = false;
 
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_CLOSE, MF_BYCOMMAND);
+        ShowWindow(GetConsoleWindow(), SW_MINIMIZE);
+
         if (args.Contains("debug"))
         {
             debug = true;
@@ -25,9 +62,10 @@ class PalmTreeTime
 
         Console.WriteLine("Ready!");
         Init();
+        Console.ReadLine();
     }
 
-    static void Init()
+    public static void Init()
     {
         maxTime = ReadReg<int>("TimeContingent");
         lastTimeUsed = DateTime.Parse(ReadReg<string>("LastTimeRun"));
@@ -41,12 +79,12 @@ class PalmTreeTime
         {
             if (currentTime >= lastTimeUsed.AddMinutes(maxTime))
             {
-                Shutdown(true, true);
+                Shutdown(true);
                 return;
             }
         }
-        checkTimer = new System.Timers.Timer(60000);
-        checkTimer.Elapsed += Timer;
+
+        checkTimer.Elapsed += OnTimedEvent;
         checkTimer.AutoReset = true;
         checkTimer.Enabled = true;
     }
@@ -97,35 +135,39 @@ class PalmTreeTime
         return;
     }
 
-    static void Timer(Object source, ElapsedEventArgs e)
+    static void OnTimedEvent(object? source, ElapsedEventArgs e)
     {
-        Console.WriteLine("Did a check.");
-        if (currentTime >= lastTimeUsed.AddMinutes(maxTime))
-            {
-                Shutdown(true);
-                return;
-            }
-    }
-    static void Shutdown(bool withWarning, bool now = false)
-    {
-        if (debug)
+        Console.Write(".");
+        if (DateTime.Now >= lastTimeUsed.AddMinutes(maxTime))
         {
-            Process.Start("notepad.exe");
-            return;
+            Shutdown(true);
         }
+    }
+    static void Shutdown(bool withWarning)
+    {
+        ShowWindow(GetConsoleWindow(), SW_SHOWNORMAL);
+        SetWindowPos(GetConsoleWindow(), new IntPtr(HWND_TOPMOST), 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 
         if (withWarning)
         {
             Console.BackgroundColor = ConsoleColor.Red;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("ZEIT ABGELAUFEN");
+            Console.WriteLine(DateTime.Now.ToString());
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("Speichere dein Spiel und drücke Enter.");
             Console.ReadLine();
         }
-        Process.Start("shutdown.exe /t 0");
-        
+
+        if (debug)
+        {
+            Process.Start("notepad.exe");
+        }
+        else
+        {
+            Process.Start("shutdown.exe", "/s /t 0");
+        }
     }
 
 }
